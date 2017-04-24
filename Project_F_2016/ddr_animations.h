d5ab07b6a314d2b901765e472bba6494424350ef
Aaron Levin <awlevin@comcast.net>
2017-04-23 23:47:56 -0500

    Get menu navigation and play-screen animation working end to end

--- Project_F_2016/ddr_animations.h
+++ Project_F_2016/ddr_animations.h
@@ -9,29 +9,29 @@
 #include "lcd.h"
 #include "ddr_images.h"
 #include "ddr_game.h"
+#include "timers.h"
+
 
 #define SEC_ONE     50000000
 #define WAIT_TIME   500000
 #define ONE_SHOT    TIMER_TAMR_TAMR_1_SHOT
-#define x_left_threshold  (0xFFF / 4) * 3
-#define y_up_threshold  	(0xFFF / 4) * 3
-#define x_right_threshold (0xFFF / 4)
-#define y_down_threshold  (0xFFF / 4)
 
-void display_welcome_screen(void);
+
+typedef enum {ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT} arrow_dir_t;
+
+// Defines an arrow that will be placed on the screen.
+// Assumes bitmaps for each arrow type are stored elsewhere
+// Assumes x coordinates for each arrow type are stored elsewhere
+typedef struct{
+	arrow_dir_t arrow_type;
+	uint16_t y_pos;
+}arrow_t;
+
+
+
 void print_arrow(arrow_t arrow);
 bool add_arrow(arrow_dir_t dir);
 bool add_two_arrows(arrow_dir_t dir1, arrow_dir_t dir2);
-void update_ui(void);
-void lcd_print_stringXY(char *msg, int8_t X,int8_t Y,uint16_t fg_color, uint16_t bg_color);
-void printMenu(); 
-void print_PlayNow(); 
-void draw_line(uint8_t y);
-void select_menuItem();
-void navigate_menu(uint16_t y_adc_data);
-
-// DEPRECATED
-// void print_arrows(void);
-// void move_arrows(int num, int dir, int speed, uint16_t fColor, uint16_t bColor);
+void animate_arrows(void);
 
 #endif
