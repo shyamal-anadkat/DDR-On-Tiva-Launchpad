6d883d58ed647518d47fbd60ad3b138967f4ed6a
Sneha Patri <snehapatri@gmail.com>
2017-04-23 23:15:14 -0500

    high scores screen

---
+++ ddr_animations.c
@@ -0,0 +1,314 @@
+#include "ddr_animations.h"
+#include "ddr_game.h"
+#include "timers.h"
+
+arrow_t arrows_on_screen[MAX_ARROWS_ON_SCREEN];
+uint8_t num_arrows_on_screen;
+bool playSelected;
+bool backSelected;
+
+//*****************************************************************************
+// Updates the entire user interface according to the current state of the game
+// Increments all of the onscreen arrows by 1
+//*****************************************************************************
+void update_ui(void) {
+	uint8_t i;
+	uint16_t y_pos;
+	
+	// TODO: Check GAMESTATE_FSM
+	
+	// MOVE ALL ONSCREEN ARROWS UP BY 1
+	for(i = 0; i < num_arrows_on_screen; i++) {
+		arrow_t* arrow = &arrows_on_screen[i];
+		arrow->y_pos++;
+		print_arrow(*arrow);
+	}
+}
+
+// RETURNS FALSE IF TOO MANY ARROWS ADDED
+bool add_arrow(arrow_dir_t dir) {
+	arrow_t arrow;
+	
+	// Instantiate arrow structs to add to arrows_on_screen array
+	arrow.arrow_type = dir;
+	arrow.y_pos = ARROW_POS_START_Y;
+	
+	// Return false if no more arrows can be added to the screen
+	if(num_arrows_on_screen >= MAX_ARROWS_ON_SCREEN) return false;
+	else {
+		arrows_on_screen[num_arrows_on_screen++] = arrow;
+		return true;
+	}
+}
+
+bool add_two_arrows(arrow_dir_t dir1, arrow_dir_t dir2) {
+	arrow_t arrow1;
+	arrow_t arrow2;
+	
+	// Instantiate arrow structs to add to arrows_on_screen array
+	arrow1.arrow_type = dir1;
+	arrow1.y_pos = ARROW_POS_START_Y;
+	arrow2.arrow_type = dir2;
+	arrow2.y_pos = ARROW_POS_START_Y;
+
+	// Return false if no more arrows can be added to the screen
+	if(num_arrows_on_screen >= MAX_ARROWS_ON_SCREEN) return false;
+	else {
+		arrows_on_screen[num_arrows_on_screen++] = arrow1;
+		arrows_on_screen[num_arrows_on_screen++] = arrow2;
+		return true;
+	}
+}
+
+// ASSUMES Y POSITION OF ARROW HAS ALREADY BEEN UPDATED
+void print_arrow(arrow_t arrow) {
+	uint16_t x_pos;
+	const uint8_t *arrow_bitmap;
+	
+	switch(arrow.arrow_type) {
+		case ARROW_UP:
+			x_pos = ARROW_POS_X_UP;
+			arrow_bitmap = up_arrowBitmaps;
+			break;
+		case ARROW_DOWN:
+			x_pos = ARROW_POS_X_DOWN;
+			arrow_bitmap = down_arrowBitmaps;
+			break;
+		case ARROW_LEFT:
+			x_pos = ARROW_POS_X_LEFT;
+			arrow_bitmap = left_arrowBitmaps;
+			break;
+		case ARROW_RIGHT:
+			x_pos = ARROW_POS_X_RIGHT;
+			arrow_bitmap = right_arrowBitmaps;
+			break;
+	}
+	
+	// DRAW ARROW
+	lcd_draw_image(
+                  x_pos,              // X Pos
+                  ARROW_WIDTH,   			// Image Horizontal Width
+                  arrow.y_pos,        // Y Pos
+                  ARROW_HEIGHT,  			// Image Vertical Height
+                  arrow_bitmap, 			// Image
+                  LCD_COLOR_BLUE,     // Foreground Color
+                  LCD_COLOR_BLACK     // Background Color
+                );
+	
+	// CLEAR GLITCHY BLUE TRAIL
+		lcd_draw_image(
+                  x_pos,              // X Pos
+                  ARROW_WIDTH,   			// Image Horizontal Width
+                  arrow.y_pos - ARROW_HEIGHT + 1,// Y Pos of bottom of image
+                  ARROW_HEIGHT,  			// Image Vertical Height
+                  arrow_bitmap, 			// DON'T CARE
+                  LCD_COLOR_BLACK,     // Foreground Color
+                  LCD_COLOR_BLACK     // Background Color
+                );
+}
+
+
+void display_welcome_screen(void) {
+			lcd_draw_image(
+                  133,                // X Pos
+                  ddrbwWidthPixels,   // Image Horizontal Width
+                  82,                	// Y Pos
+                  ddrbwHeightPixels,  // Image Vertical Height
+                  ddrbwBitmaps, 		  // Image
+                  LCD_COLOR_BLACK,    // Foreground Color
+                  LCD_COLOR_WHITE     // Background Color
+                ); 
+}
+
+
+void lcd_print_stringXY(
+    char *msg, 
+    int8_t X,
+		int8_t Y,
+    uint16_t fg_color, 
+    uint16_t bg_color
+)
+{
+	int len = strlen(msg);	// get the length of the message passed in using strlen in string.h
+	
+	int Xpixels;	// convert X to pixels to call lcd_draw_image
+	int Ypixels;  // convert Y to pixels to call lcd_draw_image
+	
+	int i;		// for the for loop
+	
+	int currentX = X;		// to preserve the current x
+	int currentY = Y;		// to preserve the current y
+	
+	int val;						// C ascii value of the character
+	int image_bitmap;			// convert it to the image bitmap from lookup-table given above
+	
+	// for loop to go through the characters in the given string
+	for(i = 0; i < len; i++) {
+		
+		// wrap around if statements
+		if(currentX == 14) {		// if the value of the current X is greater than 13
+			if(msg[i] == 32) {		// if the ascii value of space is our characte
+				i = i + 1;					// go to the next character to start the next line
+			}
+			currentX = X;					// update the currentX to the original X			
+			currentY = currentY+1;	// change Y to go to the next line
+		}
+		
+		if(currentY == 20 ) {		// if the value of current Y is greater than 19
+			currentY = 0;					// go to the 0th index of Y or simply, the top of the screen
+		}
+		
+		// get the ascii/bitmap/pixel values
+		val = (int) msg[i];			// get the ascii value of the character
+		image_bitmap = (val - 32)*48;	// convert to the bitmap value 
+		
+		Xpixels = 240 - ((currentX+1)*17 ) ;	// based on the current index, change the pixel of X. The width changes with the X coordinate. 
+		Ypixels = 320 - ((currentY+1)*16 ); 	// based on the current index, change the pixel of Y. The height changes with the Y coordinate. 
+		
+		// call lcd_draw_image, based on the image bitmap and X and Y pixels
+	lcd_draw_image(
+		Xpixels, 
+		17, 
+		Ypixels, 
+		16, 
+		&(sitkaSmall_12ptBitmaps[image_bitmap]), 
+		fg_color, 
+		bg_color
+	);
+		// increment the index of X
+		currentX++;
+	
+  }
+}
+
+void printMenu() { 
+
+	char menu[] = "MENU"; 	
+	char msg1[] = "PLAY NOW";
+	char msg2[] = "HIGH SCORES";
+	
+  lcd_clear_screen(LCD_COLOR_BLACK);
+	
+	lcd_print_stringXY(menu,0,0, LCD_COLOR_YELLOW, LCD_COLOR_BLACK);
+	lcd_print_stringXY(msg1,2,4, LCD_COLOR_YELLOW,LCD_COLOR_BLACK);
+  lcd_print_stringXY(msg2,1,7, LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+}
+
+//void print_PlayNow() {
+//	char playnow[] = "PLAY NOW" ;
+//	char score[] = "SCORE"; 
+//	lcd_print_stringXY(playnow,0, 0, LCD_COLOR_YELLOW, LCD_COLOR_BLACK);
+//	lcd_print_stringXY(score, 10,18, LCD_COLOR_YELLOW, LCD_COLOR_BLACK);
+//	
+//}
+
+void print_highscores() {
+	
+	char h[] = "HIGH";
+	char s[] = "SCORES: ";
+	char lon[] = "LONGEST";
+	char time[] = "TIME: ";
+	char game[] = "GAME";
+	char mode[] = "MODE: ";
+	char back[] = "BACK"; 
+	
+	lcd_clear_screen(LCD_COLOR_BLACK);
+	
+	lcd_print_stringXY(h, 0,4, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+	lcd_print_stringXY(s, 0,5, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+	lcd_print_stringXY(lon,0, 7, LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	lcd_print_stringXY(time, 0,8, LCD_COLOR_GREEN, LCD_COLOR_BLACK);
+  lcd_print_stringXY(game,0,10, LCD_COLOR_RED,LCD_COLOR_BLACK);
+	lcd_print_stringXY(mode, 0,11, LCD_COLOR_RED, LCD_COLOR_BLACK);
+	lcd_print_stringXY(back,0, 0, LCD_COLOR_YELLOW,LCD_COLOR_BLACK);
+
+}
+
+void draw_line(uint8_t y) {
+	char dot[] = "--------------";
+	uint8_t x = 0; 
+		lcd_print_stringXY(dot, x, y, LCD_COLOR_BLUE, LCD_COLOR_BLACK);
+}
+
+void clear_line(uint8_t y) {
+	char dot[] = "--------------";
+	uint8_t x = 0; 
+	lcd_print_stringXY(dot, x, y, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+}
+
+void draw_linehighscores1(uint8_t y) { 
+	char dot[] = "-----";
+	uint8_t x = 0; 
+		lcd_print_stringXY(dot, x, y, LCD_COLOR_BLUE, LCD_COLOR_BLACK);
+}
+
+void clear_linehighscores1(uint8_t y) {
+	char dot[] = "-----";
+	uint8_t x = 0; 
+	lcd_print_stringXY(dot, x, y, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+}
+void draw_linehighscores2(uint8_t x) {
+	char dot[] = "|";
+	uint8_t y = 0; 
+	lcd_print_stringXY(dot, x, y, LCD_COLOR_BLUE, LCD_COLOR_BLACK);
+}
+
+void clear_linehighscores2(uint8_t x) {
+	char dot[] = "|";
+	uint8_t y = 0; 
+	lcd_print_stringXY(dot, x, y, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+}
+
+void select_menuItem(int item) {
+	if(item) {
+		
+		//PLAY NOW HIGHLIGHTED
+		clear_line(6);
+		clear_line(8);
+		draw_line(3);
+		draw_line(5);
+	} else{
+		
+	 // HIGH SCORES HIGHLIGHTED 
+	 clear_line(3);
+	 clear_line(5);
+	 draw_line(6);
+	 draw_line(8);
+ }
+}
+
+void select_back(int item) {
+	if(item) {
+		draw_linehighscores1(1);
+		draw_linehighscores2(4);
+	} else {
+		clear_linehighscores1(1);
+		clear_linehighscores2(4);
+	}
+	
+	
+}
+
+void navigate_menu(uint16_t y_adc_data) {
+		
+	  if (y_adc_data >= y_up_threshold) {
+				select_menuItem(1);
+				playSelected = true;
+		} 
+		else if (y_adc_data <= y_down_threshold) {
+				select_menuItem(0);
+				playSelected = false; 
+		} 			
+	}
+
+void navigate_back(uint16_t y_adc_data) {
+		
+	  if (y_adc_data >= y_up_threshold) {
+				select_back(1);
+				backSelected = true;
+		} 
+		else if (y_adc_data <= y_down_threshold) {
+				select_back(0);
+				backSelected = false; 
+		} 			
+	}
---
+++ main.c
@@ -0,0 +1,164 @@
+ // Copyright (c) 2015-16, Joe Krachey
+// All rights reserved.
+//
+// Redistribution and use in source or binary form, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions in source form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in 
+//    the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "main.h"
+
+char group[] = "Group00";
+char individual_1[] = "Shyamal Anadkat";
+char individual_2[] = "Aaron Levin";
+char individual_3[] = "Sneha Patri";
+
+extern bool playSelected;
+extern bool backSelected;
+
+
+
+//************ENUMS********************//
+typedef enum {
+  DEBOUNCE_ONE,
+  DEBOUNCE_1ST_ZERO,
+  DEBOUNCE_2ND_ZERO,
+  DEBOUNCE_PRESSED
+}
+DEBOUNCE_STATES;
+
+//*****************************************************************************
+// DISABLE INTERRUPTS 
+//*****************************************************************************
+void DisableInterrupts(void)
+{
+  __asm {
+         CPSID  I
+  }
+}
+
+//*****************************************************************************\
+// ENABLE INTERRUPTS 
+//*****************************************************************************
+void EnableInterrupts(void)
+{
+  __asm {
+    CPSIE  I
+  }
+}
+
+//*****************************************************************************
+// INIT HARDWARE AND PERIPHERALS 
+//*****************************************************************************
+void initialize_hardware(void)
+{
+	//enable interrupts
+	EnableInterrupts();
+	
+	//init serial debug for printf
+	init_serial_debug(true, true);
+	
+	//init leds and switches
+	lp_io_init();
+
+	//LCD init sequence: gpio + screen config 
+	lcd_config_gpio();
+	lcd_config_screen();
+	
+	// Initialize the TIMER0 to be a 
+  //      32-bit
+  //      one-shot
+  //      count down
+  //      no interrupts
+  gp_timer_config_32(TIMER0_BASE, ONE_SHOT, false, false);
+	
+	ps2_initialize(); 
+	
+	//disable interrupts
+	DisableInterrupts();
+}
+
+
+int debounce_sw1(void) {
+  static uint16_t sw1_count = 0;
+	bool return_value = false;
+	
+	
+	if (lp_io_read_pin(SW1_BIT)) {
+		sw1_count = 0;
+	} else if (sw1_count <= 6) {
+		sw1_count++;
+	}
+	
+  if (!lp_io_read_pin(SW1_BIT)) {
+		sw1_count++;
+  } else {
+		sw1_count = 0;
+	}
+	return sw1_count == 6;
+}
+
+
+//*****************************************************************************
+// MAIN
+//*****************************************************************************
+int 
+main(void)
+{
+	uint16_t x_adc_data, y_adc_data;
+  char msg[80];
+  initialize_hardware();
+	
+	printf("\n\r");
+  printf("**************************************\n\r");
+  printf("* ECE353 - Final Project - Debug\n\r");
+  printf("**************************************\n\r");
+  printf("\n\r");
+	
+	printMenu(); 
+	select_menuItem(1);
+	playSelected = true; 
+	
+  while(1){
+		
+		//get x and y adc values 
+		x_adc_data = ps2_get_x();
+    y_adc_data = ps2_get_y();
+		
+		//print x,y adc values for debug
+    //printf("X Dir value : 0x%03x  Y Dir value : 0x%03x\r",x_adc_data, y_adc_data);
+
+		//navigate menu.todo: make stateful 	
+	  navigate_menu(y_adc_data);
+		
+		if(debounce_sw1()) {
+			if(playSelected) {
+				//printf("PRESSED: IN PLAY\n");
+			} else {
+				//printf("IN HIGH SCORE MODE\n");
+					print_highscores();
+					navigate_back(y_adc_data);
+				
+					if(debounce_sw1()) {
+							if(backSelected) {
+								printf("PRESSED: IN MENU\n");
+							}
+					}
+			}
+		}
+	}
+}
