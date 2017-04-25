c495795598d6c52c2a4082c24eeb499b2393890b
Aaron Levin <awlevin@comcast.net>
2017-04-25 16:58:15 -0500

    Decode every IO expander button combination
    Also adds a top-arrow initializer; these could be globals to reprint faster.

--- peripherals/include/mcp23017.h
+++ peripherals/include/mcp23017.h
@@ -43,7 +43,42 @@
 #define 	IO_BUTTON_DIR 0x01 
 
 
-typedef enum {BTN_UP, BTN_DOWN, BTN_LEFT, BTN_RIGHT, NONE} button_dir_t;
+//*****************************************************************************
+// BUTTON COMBINATION MACROS
+//*****************************************************************************
+#define BTN_NONE	0x0F
+#define BTN_U			0x0E
+#define BTN_D			0x0D
+#define BTN_L			0x0B
+#define BTN_R			0x07
+#define	BTN_UD		0x0C
+#define BTN_UL		0x0A
+#define BTN_UR		0x06
+#define BTN_LR		0x03
+#define BTN_LD		0x09
+#define BTN_DR		0x05
+
+
+//*****************************************************************************
+// PORT EXPANDER CONSTANTS AND ENUMS
+//*****************************************************************************
+
+typedef enum {
+	NONE, BTN_UP, BTN_DOWN, BTN_LEFT, BTN_RIGHT,
+	BTN_UP_DOWN, BTN_UP_LEFT, BTN_UP_RIGHT,
+	BTN_LEFT_RIGHT, BTN_LEFT_DOWN, BTN_DOWN_RIGHT
+} button_dir_t;
+
+typedef enum {
+  DEBOUNCE_ONE,
+  DEBOUNCE_1ST_ZERO,
+  DEBOUNCE_2ND_ZERO,
+  DEBOUNCE_PRESSED,
+	DEBOUNCE_DONE
+}
+DEBOUNCE_STATES;
+
+
 
 //*****FUNCTION DECLARATIONS*****
 bool ioexpander_init(); //default : I2C1 BASE
@@ -60,6 +95,6 @@ void turn_off_leds(void);
 
 void control_leds(uint8_t data);
 
-button_dir_t is_button_pressed();
+button_dir_t buttons_pressed();
 
 #endif
